366d3724a759bee0d16a5136a1d23d5809ae0130
Aldii24 <aldiirawan748@gmail.com>
2024-03-10 19:42:59 +0700

    update-search

--- index.js
+++ index.js
@@ -6,8 +6,21 @@ menu.onclick = () => {
 }
 
 document.addEventListener('click', function (e) {
-    console.log(e)
     if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
     }
 });
+
+const btn = document.getElementById('search-btn');
+btn.onclick = () => {
+    const search = document.getElementById('search').value;
+    if (search.trim() !== '') {
+        window.location.href = `https://www.google.com/search?q=${search}`
+    }
+}
+function searchFunction(event) {
+    const search = document.getElementById('search').value;
+    if (event.key === 'Enter' && search.trim() !== '') {
+        window.location.href = `https://www.google.com/search?q=${search}`
+    }
+}
